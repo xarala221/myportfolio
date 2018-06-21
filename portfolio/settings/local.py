e0c21b8c450dcf2bbda600d24039b0b133070b42
xarala221 <xaralaxarala@gmail.com>
2018-06-21 21:18:42 +0000

    Change local and base

--- portfolio/settings/local.py
+++ portfolio/settings/local.py
@@ -83,7 +83,10 @@ DATABASES = {
     }
 }
 
+import dj_database_url
 
+db_from_env = dj_database_url.config()
+DATABASES['default'].update(db_from_env)
 # Password validation
 # https://docs.djangoproject.com/en/2.0/ref/settings/#auth-password-validators
 
