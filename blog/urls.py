043a19feb9a0f4ee9546ecca8896d96e6185337f
xarala221 <xaralaxarala@gmail.com>
2018-06-22 11:00:51 +0000

    edit urls

--- blog/urls.py
+++ blog/urls.py
@@ -8,5 +8,4 @@ urlpatterns = [
   url(r"^create/$", new_post, name="create"),
   url(r"^detail/(?P<id>[0-9]+)/edit/$", edit_post, name="update"),
   url(r"^detail/(?P<id>[0-9]+)/delete/$", delete_post, name="delete"),
-  url(r'^tinymce/', include('tinymce.urls')),
 ]
