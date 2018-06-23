7d50a5493c88139544553acdb8534639941b0951
xarala221 <xaralaxarala@gmail.com>
2018-06-23 14:27:13 +0000

    static root

--- portfolio/settings/production.py
+++ portfolio/settings/production.py
@@ -124,11 +124,11 @@ STATICFILES_DIRS = [
     #'/var/www/static/',
 ]
 
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static_cdn")
+STATIC_ROOT = os.path.join(BASE_DIR,"live-static", "static-root")
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 MEDIA_URL = "/media/"
-MEDIA_ROOT = os.path.join(os.path.dirname(BASE_DIR), "media_cdn")
+MEDIA_ROOT = os.path.join(BASE_DIR, "live-static" "media-root")
 
 
 #django_heroku.settings(locals())
-STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
