634202e264c20ca5e166bcea7a87ef3d1dd93595
xarala221 <xaralaxarala@gmail.com>
2018-06-23 13:25:38 +0000

    urls.py edit

--- blog/urls.py
+++ blog/urls.py
@@ -1,7 +1,7 @@
 from django.conf.urls import url, include
 
 from blog.views import *
-#app_name = 'blog'
+
 urlpatterns = [
   url(r"^$", all_post, name="list"),
   url(r"^create/$", new_post, name="create"),
@@ -9,5 +9,3 @@ urlpatterns = [
   url(r"^(?P<slug>[^\.]+).html/delete$",  delete_post, name="delete"),
   url(r"^(?P<slug>[^\.]+).html/$", post_detail, name="blog-detail"),
 ]
-
- #url(r"^detail/(?P<id>[0-9]+)/delete/$", delete_post, name="delete"),
